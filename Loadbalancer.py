2ab17d3d7c42ff40b8a89da51fd092c48404e81f
simo <simobeneckert@gmail.com>
2024-05-06 19:56:46 +0200

    Updated the code to receive JSON data, determine the server type and forward the data to the appropriate server

--- Loadbalancer.py
+++ Loadbalancer.py
@@ -1,20 +1,29 @@
 import socket
 import threading
+import json
 
 LOAD_BALANCER_HOST = "loadbalancer"
 LOAD_BALANCER_PORT = 9999
 SERVERS = {"TCP-Server": 8888, "UDP-Server": 8889}
 
-def connect_to_server(server_adress, server_type, client_socket,):
+def connect_to_server(server_adress, server_type, rest_data, connect_necessary):
     server_socket = socket.socket(socket.AF_INET, server_type)
-    server_socket.connect(server_adress)
+    
+    if(connect_necessary):
+        server_socket.connect(server_adress)
+    
+    data_json = json.dumps(rest_data)
+    server_socket.send(data_json.encode())
 
-def get_server_by_name(server_name):
+    server_socket.close()
+
+
+def get_server_by_name(server_name, rest_data):
     match server_name:
         case "TCP-Server":
-            connect_to_server(SERVERS[server_name], socket.SOCK_STREAM)
+            connect_to_server(SERVERS[server_name], socket.SOCK_STREAM, rest_data, True)
         case "UDP-Server":
-            connect_to_server(SERVERS[server_name], socket.SOCK_DGRAM)
+            connect_to_server(SERVERS[server_name], socket.SOCK_DGRAM, False)
 
 def receive_from_client():
     load_balancer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,7 +37,9 @@ def receive_from_client():
         print(f"Accepted connection from {client_address}")
 
         client_data = client_socket.recv(1024).decode()
+        data_dict = json.loads(client_data)
 
         print("Received data from client:", client_data)
         
         client_socket.close()
+        get_server_by_name(data_dict.pop("Connect to"), data_dict)
