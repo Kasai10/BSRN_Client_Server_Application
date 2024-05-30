98914b0ecf3adae3cbb31e77fd512423fa9232ed
afzalizohra <zohra.afzali@outlook.de>
2024-05-30 15:10:48 +0200

    added functions to get port number of the load balancer and to get the hostname

--- Client.py
+++ Client.py
@@ -3,9 +3,33 @@ import json
 
 def get_load_balancer_ip():
     while True:
-        load_balancer_ip: input("Enter the IP address of the load balancer: ") # type: ignore
+        load_balancer_ip: input("Enter the IP address of the load balancer: ")  # type: ignore
         try:
             socket.inet_aton(load_balancer_ip)
             return load_balancer_ip
         except socket.error:
             print("Invalid IP address. Please enter a valid IP address")
+
+
+def get_load_balancer_port():
+    while True:
+        port_input: input("Enter the port number of the load balancer: ")  # type: ignore
+        try:
+            load_balancer_port = int(port_input)
+            return load_balancer_port
+        except ValueError:
+            print("Invalid input for port number. Please enter a valid number")
+
+
+
+def get_hostname_from_ip(load_balancer_ip):
+    try:
+        hostname = socket.gethostbyname(load_balancer_ip)
+        print (f"The hostname for the IP address {load_balancer_ip} is {hostname}")
+        return hostname
+    except socket.gaierror as e:
+        print(f"Error converting IP address: {e} ")
+        return None
+        
+        
+        
