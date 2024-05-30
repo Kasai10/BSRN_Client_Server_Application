887cd2f2e809e8420625abbb8471dc015d27c421
tahira-fellermeier <167790134+tahira-fellermeier@users.noreply.github.com>
2024-05-30 16:16:22 +0200

    do_GET and do_PUT

--- tcp-server.py
+++ tcp-server.py
@@ -8,4 +8,19 @@ class requestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Hello, world!")
 
+    def do_PUT(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        logging.info(f"PUT request received from {self.client_address}, Payload: {post_data.decode()}")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Received PUT request")
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        post_data_decoded = post_data.decode()
+        logging.info(f"POST request received from {self.client_address}, Payload: {post_data_decoded}")  
+
+
     
