bc31ec43c07a910dea0a3432d3d8c1514471c07f
simo <simobeneckert@gmail.com>
2024-06-01 15:05:32 +0200

    Properly added forwarding to the http server as well as fixing missing ip address

--- Loadbalancer.py
+++ Loadbalancer.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import json
+import http.client
 
 LOAD_BALANCER_HOST = "loadbalancer"
 LOAD_BALANCER_PORT = 9999
@@ -11,40 +12,54 @@ lock_thread = threading.Lock()
 def connect_to_server(server_adress, server_type, rest_data, connect_necessary):
     server_socket = socket.socket(socket.AF_INET, server_type)
     
-    if(connect_necessary):
-        server_socket.connect(server_adress)
-    
     data_json = json.dumps(rest_data)
-    server_socket.send(data_json.encode())
+
+    if(connect_necessary):
+        conn = http.client.HTTPConnection(server_adress[0], server_adress[1])
+        headers = {'Content-type': 'application/json'}
+        method = rest_data.pop('Method')
+
+        if method == 'GET':
+            conn.request(method, '/', headers=headers)
+        else:
+            conn.request(method, '/', body=data_json, headers=headers)
+        
+        response = conn.getresponse()
+        print(f"Response status: {response.status}, reason: {response.reason}")
+        conn.close()
+    else:
+        server_socket.sendto(data_json.encode(),server_adress)
+    
 
     server_socket.close()
 
 
-def get_server_by_name(server_name, rest_data):
+def get_server_by_name(server_name, server_address, rest_data):
     with lock_thread:
         if not SERVERS[server_name]:
             raise Exception(f"No available ports for {server_name}")
         
         server_port = SERVERS[server_name].pop(0)
+        server_address = (server_address, server_port)
     
     try:
         match server_name:
             case "TCP-Server":
-                connect_to_server(server_port, socket.SOCK_STREAM, rest_data, True)
+                connect_to_server(server_address, socket.SOCK_STREAM, rest_data, True)
             case "UDP-Server":
-                connect_to_server(server_port, socket.SOCK_DGRAM, False)
+                connect_to_server(server_address, socket.SOCK_DGRAM, rest_data, False)
     except:
         raise Exception(f"Connecting to {server_name} unsuccessfull")
     finally:
         with lock_thread:
             SERVERS[server_name].append(server_port)
 
-def handle_client_connection(client_socket, client_address):
+def handle_client_connection(client_socket):
     client_data = client_socket.recv(1024).decode()
     data_dict = json.loads(client_data)
     print("Received data from client:", client_data)
     client_socket.close()
-    get_server_by_name(data_dict.pop("Connect to"), data_dict)
+    get_server_by_name(data_dict.pop("Connect to"), data_dict.pop("IP-Adress"),data_dict)
     
 
 def receive_from_client():
