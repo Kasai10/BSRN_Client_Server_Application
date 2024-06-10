ecc0fa69c5c6340613871838225715ec65e665fc
simo <simobeneckert@gmail.com>
2024-06-10 08:42:33 +0200

    Resolved complicated merge conflict by resetting test-branch. This is Zohras work

--- Client.py
+++ Client.py
@@ -6,6 +6,8 @@ LOAD_BALANCER_PORT = 9999
 UDP_SERVER_PORT = 8887
 BUFFER_SIZE = 4096
 
+def print_header(header):
+    print(f"\n--- {header} ---\n")
 
 def get_server_type():
     while True:
@@ -30,39 +32,46 @@ def get_http_method(server_type):
 def get_payload():
     server_type = get_server_type()
     method = get_http_method(server_type)
-    message = input("Enter the message: ")
+    message = ""
+
+    if method not in ["GET", "DELETE"]:
+        message = input("Enter the message: ")
+    
     payload = json.dumps({"Connect to": server_type, "Message": message, "Method": method})
-    return payload
+    return payload, server_type
 
 
 def send_message():
     try:
-        payload = get_payload()
-        communicate_with_load_balancer(payload)
+        payload, server_type = get_payload()
+        communicate_with_load_balancer(payload, server_type)
     except Exception as e:
         print(f"An error has occurred: {e}")
 
 
 
-def communicate_with_load_balancer(payload):
+def communicate_with_load_balancer(payload, server_type):
     try:
         tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcp_socket.connect((LOAD_BALANCER_HOST, LOAD_BALANCER_PORT))
         tcp_socket.sendall(payload.encode())
-
+        print("[INFO] Payload sent to the load balancer.")
+        
         response = receive_response_from_tcp_server(tcp_socket)
         if response is not None:
-            print("Response from the TCP server:")
+            print("[INFO] Response from the TCP server:")
             print(response)
+        if "UDP" in server_type:
+            receive_response_from_udp_server()
     except Exception as e:
-        print(f"Error communicating to the load balancer: {e}")
+        print(f"Payload couldn't be sent to the load balancer: {e}")
     finally:
         tcp_socket.close()
 
 
 def receive_response_from_tcp_server(tcp_socket):
     try:
-        response = tcp_socket.recv(1024).decode()
+        response = tcp_socket.recv(1024).decode
         return response
     except socket.error as e:
         print(f"Error receiving the response from the  TCP server: {e}")
@@ -73,7 +82,7 @@ def receive_response_from_tcp_server(tcp_socket):
 def receive_response_from_udp_server():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(('', UDP_SERVER_PORT))
-    print(f"Listening for UDP response on port {UDP_SERVER_PORT}...")
+    print(f"[INFO] Listening for UDP response on port {UDP_SERVER_PORT}...")
 
     try:
         while True:
@@ -81,12 +90,10 @@ def receive_response_from_udp_server():
             decoded_message = message.decode()
             try:
                 json_response = json.loads(decoded_message)
-                print(f"Response from the UDP server:")
-                print(json.dumps(json_response, indent=4))
+                print("[INFO] Response from the UDP server:")
+                print(json.dumps(json_response))
             except json.JSONDecodeError as e:
                 print(f"Error decoding the JSON response: {e}")
-                print(f"Raw response from UDP server: {decoded_message}")
-
     except socket.error as e:
         print(f"Error receiving the response from the UDP server: {e}")
     finally:
@@ -95,10 +102,14 @@ def receive_response_from_udp_server():
 
 
 if __name__ == "__main__":
-    send_message()
-    receive_response_from_udp_server()
+    print_header("Client Application Started")
+    while True:
+        send_message()
+        continue_sending_requests = input("\nDo you want to send another request (yes/no)? ").strip().lower()
+        if continue_sending_requests == "yes":
+            print("\n")
+        else:
+            print_header("Program Ended")
+            break
 
-
-        
-        
-        
+    
