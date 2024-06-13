44b0fe5424cfc3f0da28acbf4e65a37b8e7638b6
tahira-fellermeier <167790134+tahira-fellermeier@users.noreply.github.com>
2024-06-13 15:31:08 +0200

    Removed redundancy in start_logging

--- tcp-server.py
+++ tcp-server.py
@@ -5,47 +5,41 @@ import json
 
 class request_handler(BaseHTTPRequestHandler): 
     def do_GET(self):
-        antwort = "The resource was successfully retrieved." 
-        self.handle_payload("GET", antwort)
+        self.handle_payload("GET", "The resource was successfully retrieved.")
 
     def do_PUT(self):
-        antwort = "The resource was successfully updated."
-        self.handle_payload("PUT", antwort)
+        self.handle_payload("PUT", "The resource was successfully updated.")
 
     def do_POST(self):
-        antwort = "New resource successfully created."
-        self.handle_payload("POST", antwort)
+        self.handle_payload("POST", "New resource successfully created.")
 
     def do_DELETE(self):
-        antwort = "The resource was successfully deleted."
-        self.handle_payload("DELETE", antwort)
+        self.handle_payload("DELETE", "The resource was successfully deleted.")
 
-    def handle_payload(self, method, antwort):
+    def handle_payload(self, method, response_message):
         try:
             if method in ['PUT', 'POST']:
                 message_length = int(self.headers['Content-Length'])
-                payload = self.rfile.read(message_length).decode()
-
-                payload_dict = json.loads(payload)
-                payload = f"- Message: {payload_dict['Message']} \n- Method: {payload_dict['Method']}"
-                
-                self.respond_to_client(method, antwort, payload)
+                payload_json = self.rfile.read(message_length).decode()
+                payload_dict = json.loads(payload_json)
+                payload = f"- Message: {payload_dict['Message']} \n- Method: {payload_dict['Method']}"          
             else:
                 payload = f"- Message: none \n- Method: {method}"
-                self.respond_to_client(method, antwort, payload)
-        except (ValueError, UnicodeDecodeError):  
-            error_info = f"Error processing request: {method}, Path: {self.path}"
+
+            self.respond_to_client(method, response_message, payload)
+        except (ValueError, UnicodeDecodeError, json.JSONDecodeError) as e:  
+            error_info = f"Error processing {method} request: {e}"
             logging.error(error_info)
             print(error_info)
             self.send_error(400, "Bad Request") 
 
-    def respond_to_client(self, method, antwort, payload):
+    def respond_to_client(self, method, response_message, payload):
         try:
             print(f"Incoming {method} message from Client")
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            response = (f"TCP-Server received following request: \n{payload} \n{antwort}")
+            response = (f"TCP-Server received following request: \n{payload} \n{response_message}")
             self.wfile.write(response.encode())
 
             sent_info = f"Following message sent to client: \n{response}"
@@ -64,7 +58,7 @@ class request_handler(BaseHTTPRequestHandler):
         finally:
             self.wfile.flush()  # Ensure all data is flushed before closing the connection
             print("")
-            
+           
  
     def log_request(self, code='-', size='-'):
         status_info = f"Request currently being handled: \n- Client IP Address: {self.client_address[0]} \n- Request details: {self.requestline}"
@@ -89,20 +83,16 @@ def start_server():
         print(error_status)
     finally:
         httpd.server_close()
-
-    close_info = f"TCP Server is closed"    
-    print(close_info)
-    logging.info(close_info)
-
+        close_info = f"TCP Server is closed"    
+        print(close_info)
+        logging.info(close_info)
 
 def start_logging(log_file_path=None):
-    if log_file_path:
-        logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
-        start_info = "TCP Server started logging"
-        logging.info(start_info)
-        print(start_info)
-    else:
-        logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+    logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+    start_info = "TCP Server started logging"
+    logging.info(start_info)
+    print(start_info)
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="TCP Server")
@@ -110,4 +100,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     start_logging(args.logfile)
     start_server()
+
 
