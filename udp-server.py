01ce48d7b923ea82e3e188ab0bcdc19b7d4e0fe4
Seam Sultan <seam.sultan@fra-uas.de>
2024-06-23 08:56:29 +0200

    Print markierungen für orientierung gelöscht

--- udp-server.py
+++ udp-server.py
@@ -8,7 +8,6 @@ def server(port): #hier mach ich den Port als parameter mal lieber in eine funkt
     buffersize = 4096 # Hier wird dann die Buffer size deklariert in höhe von 4096 bytes, da dies eine standartmässige verwendung für UDP Datagramme hat und ich die Puffregrösse definitv auch für die recvform methode brauchen werde
     try:
         socket1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Hier wird dann halt das Socket als UDP Socket erstellt, der IPv4-Adressem benutzt.
-        print("Benachrichtigung eines neuen erstellten sockets")
         logging.info ("Ein neues Socket wurde erstellt") #Für die bessere Übersicht dann einfach nh response, dass das Socket erstellt worden ist, wenn alles glatt läuft
     except Exception as socketfehler:
         logging.error(f"Das Erstellen des Sockets mit Port : {port} hat nicht funktioniert")
@@ -17,7 +16,6 @@ def server(port): #hier mach ich den Port als parameter mal lieber in eine funkt
         adresse=('localhost', port) #Hier deklariere ich eine neue variable, die einfach die server adresse ist. Port wie gesagt, die portnummer, die in der funktion vom user deklariert wird und dann ein leerer string, wodurch dann alle IP-Adressen eine Verbindung zum Server aufbauen können. Ausserdem ist ein Tupel hier glaube ich erforderlich, weil die methode bind soweit ich weiss nur bei tupel funktioniert
         socket1.bind(adresse) #Damit das Betriebssystem dann weiss, dass alle packets, die an dem vom user gegebenen Ports gesendet werden auch an das Socket weitergeschickt werden sollen.
         logging.info(f"Socket wurde an die Adresse {adresse} gebunden")
-        print("Benachrichtigung, dass das socket an die adresse gebindet wurde")
     except Exception as bindfehlgeschlagen:
          logging.error(f"Das Binden des Sockets an die Adresse {adresse} hat nicht geklappt")
          return
@@ -39,7 +37,6 @@ def server(port): #hier mach ich den Port als parameter mal lieber in eine funkt
                 
         try:
             socket1.sendto(antwort.encode(), adressloadbalancer) 
-            print("Die nachricht sollte nun versendet worden sein an den Client")
             logging.info(f"Die Antwort {antwort} wurden an {adressloadbalancer} gesendet")
         except Exception as sendefehler:
             logging.error("Die Antwort konnte nicht versendet werden")    
