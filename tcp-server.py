06c660f74a6e8dc969a4099cf62f1e06d09c9e9b
tahira-fellermeier <167790134+tahira-fellermeier@users.noreply.github.com>
2024-06-28 15:07:04 +0200

    Added comments for clarification

--- tcp-server.py
+++ tcp-server.py
@@ -3,83 +3,114 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import argparse
 import json
 
+# Define a request handler class that inherits from BaseHTTPRequestHandler
 class RequestHandler(BaseHTTPRequestHandler): 
+    
+    # Handle GET requests
     def do_GET(self):
         self.handle_payload("GET", "The resource was successfully retrieved.")
 
+    # Handle POST requests
     def do_POST(self):
         self.handle_payload("POST", "New resource successfully created.")
 
+    # Handle PUT requests
     def do_PUT(self):
         self.handle_payload("PUT", "The resource was successfully updated.")
 
+    # Handle DELETE requests
     def do_DELETE(self):
         self.handle_payload("DELETE", "The resource was successfully deleted.")
 
+    # Common method to handle the payload from each request type
     def handle_payload(self, method, response_message):
         try:
+            # Log incoming request
             logging.info(f"Incoming {method} message from Client")
             print(f"{method} message recieved from Client")
+
+            # Read and process payload for PUT and POST Requests
             if method in ['PUT', 'POST']:
                 message_length = int(self.headers['Content-Length'])
                 payload_json = self.rfile.read(message_length).decode()
                 payload_dict = json.loads(payload_json)
                 payload = f"- Message: {payload_dict['Message']} \n- Method: {payload_dict['Method']}"          
+            
+            # Process payload for GET and DELETE Requests
             else:
                 payload = f"- Message: none \n- Method: {method}"
-
+            
+            # Calls respond_to_client() Method
             self.respond_to_client(method, response_message, payload)
+
         except (ValueError, UnicodeDecodeError, json.JSONDecodeError) as e:  
             logging.error(f"Error processing {method} request: {e}")
             self.send_error(400, "Bad Request") 
 
+    # Sends a response to the Client
     def respond_to_client(self, method, response_message, payload):
         try:
+            # Send 200 Ok response
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
+
+            # Create response message
             response = f"TCP-Server received following request: \n{payload} \n{response_message}"
             self.wfile.write(response.encode())
+
+            # Log response to Client
             logging.info(f"Following message sent to client: \n{response}")
             print(f"{response_message}\n")
+
+        # Log error if connection is reset by client before answer is possible
         except ConnectionResetError:
             logging.error(f"Connection reset by client during {method} request")
             self.send_error(500, "Connection reset by client")
         except Exception as e:
             logging.error(f"Error responding to {method} request: {e}")
             self.send_error(500, "Internal Server Error")
-        finally:
-            self.wfile.flush()  # Ensure all data is flushed before closing the connection
+        #finally:
+        #   self.wfile.flush()  # Ensure all data is flushed before closing the connection
  
+    # Override and personalise log_request method
     def log_request(self, code='-', size='-'):
         logging.info(f"Request currently being handled: \n- Client IP Address: {self.client_address[0]} \n- Request details: {self.requestline}")
 
+# start server
 def start_server():
     server_address = ('localhost', 8000)
     httpd = HTTPServer(server_address, RequestHandler)
+
+    # Log server start
     start_info = f"TCP server has started on port 8000"
     logging.info(start_info)
     print(start_info)
 
+    # Run server indefinitely 
     try:
         httpd.serve_forever()
+    
+    # Server shutdown by user (ctrl+c)
     except KeyboardInterrupt:
         logging.info("Server shutdown initiated by user")
     except (OSError) as e:
         logging.error(f"Server startup error: {e}")
     except Exception as e:
         logging.error(f"Unexpected error during server startup: {e}")
+    
+    # Close server, log that the server is closed
     finally:
         httpd.server_close()
         logging.info(f"TCP Server is closed")   
 
  
-
+# Start logging
 def start_logging(log_file_path=None):
     logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
     logging.info("TCP Server started logging")
     
-
+# Main
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="TCP Server")
     parser.add_argument('-logdatei', dest="logfile", type=str, required=True, help='Logdatei Pfad und Name')
